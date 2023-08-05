3665c5c4ac81365d1162ea1642f2b53fc2730567
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-08-05 12:49:13 -0300

    ejercicio 4 realizado.

--- src/News.jsx
+++ src/News.jsx
@@ -1,16 +1,17 @@
 import NewsItem from './NewsItem';
+import Novedades from './data/Novedades';
 
-const Novedades = () => {
+const News = () => {
   return (
     <div className="container px-4 py-5" id="featured-3">
       <h2 className="pb-2 border-bottom text-white">NOVEDADES</h2>
       <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+        {Novedades.map((item) => (
+          <NewsItem key={item.id} data={item} />
+        ))}
       </div>
     </div>
   );
 };
 
-export default Novedades;
+export default News;
--- src/NewsItem.jsx
+++ src/NewsItem.jsx
@@ -1,12 +1,8 @@
-const NovedadesItem = () => {
+const NovedadesItem = ({ data }) => {
   return (
-    <div className="feature col text-white">
-      <h3 className="fs-2">Featured title</h3>
-      <p>
-        Paragraph of text beneath the heading to explain the heading. We'll add
-        onto it with another sentence and probably just keep going until we run
-        out of words.
-      </p>
+    <div className="feature col text-white" id={`{data.id}`}>
+      <h3 className="fs-2">{data.title}</h3>
+      <p>{data.description}</p>
       <a href="#" className="text-warning">
         Call to action
       </a>
